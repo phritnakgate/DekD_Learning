b3542a381c3bb6c20f53046cba32d85bc0b46107
Phrit Nakgate <boss5500216@gmail.com>
2026-02-19 15:24:48 +0700

    ui(image): add birthday card background

--- app/src/main/java/com/example/dekd_learning/MainActivity.kt
+++ app/src/main/java/com/example/dekd_learning/MainActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,25 +48,35 @@ fun HappyBirthdayCard(
     name: String,
     sender: String
 ){
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(20.dp)
-    ) {
-        Text(
-            text = "Happy Birthday $name !",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            lineHeight = 32.sp,
-            textAlign = TextAlign.Center
+    Box {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
-        Text(
-            text = "From...$sender",
-            fontSize = 18.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
-        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
 
+            Text(
+                text = "Happy Birthday $name !",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 32.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "From...$sender",
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
+            )
+        }
     }
 }
 
