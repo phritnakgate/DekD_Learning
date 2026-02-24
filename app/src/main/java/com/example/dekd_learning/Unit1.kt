e85d4545bf1105473d01f20b13be06a38528ee05
Phrit Nakgate <boss5500216@gmail.com>
2026-02-24 23:21:51 +0700

    ui(unit2): exercise1 dice rolling app

---
+++ app/src/main/java/com/example/dekd_learning/Unit1.kt
@@ -0,0 +1,284 @@
+package com.example.dekd_learning
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HappyBirthdayCard(
+    modifier: Modifier = Modifier,
+    name: String,
+    sender: String
+){
+    Box {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+
+            Text(
+                text = stringResource(R.string.happy_birthday_receiver, name),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 32.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = stringResource(R.string.happy_birthday_sender, sender),
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End)
+            )
+        }
+    }
+}
+
+//Exercise 1: Compose Article
+@Composable
+fun ComposeArticle(modifier: Modifier = Modifier){
+    Column() {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(R.string.article_title),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.article_p1),
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.article_p2),
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+//Exercise 2: Task Manager
+@Composable
+fun TaskManager(modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.task_manager_text1),
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.task_manager_text2),
+            fontSize = 16.sp
+        )
+    }
+}
+
+//Exercise 3: Compose Quadrant
+@Composable
+fun ComposeQuadrant(modifier: Modifier = Modifier){
+    Column() {
+        Row(
+            modifier = Modifier
+                .weight(0.5F)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .background(color = colorResource(R.color.quadrant1_bg))
+                    .weight(0.5F)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.quadrant_1_heading),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.quadrant_1_desc),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .background(color = colorResource(R.color.quadrant2_bg))
+                    .weight(0.5F)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.quadrant_2_heading),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.quadrant_2_desc),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .weight(0.5F)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .background(color = colorResource(R.color.quadrant3_bg))
+                    .weight(0.5F)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.quadrant_3_heading),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.quadrant_3_desc),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .background(color = colorResource(R.color.quadrant4_bg))
+                    .weight(0.5F)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.quadrant_4_heading),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.quadrant_4_desc),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+}
+
+//Business Card
+@Composable
+fun BusinessCard(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 64.dp)
+    ) {
+        Spacer(
+            modifier = Modifier
+                .weight(1f)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(
+                modifier = Modifier.background(colorResource(R.color.business_android_bg))
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.android_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                )
+            }
+            Text(
+                text = stringResource(R.string.business_name),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Light
+            )
+            Text(
+                text = stringResource(R.string.business_position),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.business_dark_green)
+            )
+        }
+        Spacer(
+            modifier = Modifier
+                .weight(1f)
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            ContactCard(stringResource(R.string.business_tel), Icons.Rounded.Call)
+            ContactCard(stringResource(R.string.business_contact), Icons.Rounded.Share)
+            ContactCard(stringResource(R.string.business_email), Icons.Rounded.Email)
+        }
+    }
+}
+@Composable
+fun ContactCard(text: String, icon: ImageVector){
+    Row(){
+        Icon(
+            icon,
+            contentDescription = null,
+            tint = colorResource(R.color.business_dark_green),
+            modifier = Modifier
+                .padding(end = 24.dp)
+        )
+        Text(
+            text = text,
+            fontSize = 14.sp,
+        )
+    }
+}
