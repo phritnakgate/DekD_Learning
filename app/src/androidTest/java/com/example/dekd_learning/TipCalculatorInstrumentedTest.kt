b4c4d6f3713446119072ec69aa23a1662b4aafaa
Phrit Nakgate <boss5500216@gmail.com>
2026-02-26 15:09:25 +0700

    test(unit2): tip calculator instrumental test

---
+++ app/src/androidTest/java/com/example/dekd_learning/TipCalculatorInstrumentedTest.kt
@@ -0,0 +1,36 @@
+package com.example.dekd_learning
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.dekd_learning.ui.theme.DekD_LearningTheme
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+@RunWith(AndroidJUnit4::class)
+class TipCalculatorInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculateTipTest() {
+        composeTestRule.setContent {
+            DekD_LearningTheme() {
+                TipCalculator()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+
+        composeTestRule.onNodeWithText("Total Amount: $12.00").assertExists()
+    }
+}
